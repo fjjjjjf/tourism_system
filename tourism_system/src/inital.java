87ee157570366873ae827037f38055091eb2d432
fjjjjjf <106924689+fjjjjjf@users.noreply.github.com>
2022-06-24 02:01:23 +0800

    init

---
+++ tourism_system/src/inital.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class inital {
+        static final String DB_URL = "jdbc:mysql://localhost:3306/toursim_system?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        static final String USER = "root";
+        static final String Password ="q13506082361";
+
+        JFrame jf;
+
+
+}
